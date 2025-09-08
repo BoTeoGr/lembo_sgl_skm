b096932e1683e34cf59b2e9969034e5ee491caa7
stefanny834 <skaylopez2904@gmail.com>
2025-09-08 18:42:28 -0500

    las opciones se muestran de acuerdo al rol, mejore la UX

--- frontend/public/js/pages/CiclosCultivos.js
+++ frontend/public/js/pages/CiclosCultivos.js
@@ -165,6 +165,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
 
   function showCicloCultivoDetails(ciclo) {
+    document.getElementById('modalCicloCultivoId').textContent = ciclo.id || '';
     document.getElementById('modalCicloCultivoNombre').textContent = ciclo.nombre || '';
     document.getElementById('modalCicloCultivoDescripcion').textContent = ciclo.descripcion || '';
     document.getElementById('modalCicloCultivoNovedades').textContent = ciclo.novedades || '';
